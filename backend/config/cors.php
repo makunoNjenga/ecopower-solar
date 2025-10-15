e269f678c4e5beda1daf5da6c6e555dc1dfca460
Simon Njenga <makuno.biz@gmail.com>
2025-10-15 06:47:38 +0300

    Add frontend URL to CORS allowed origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,12 +19,14 @@ return [
 
     'allowed_methods'          => ['*'],
 
-    'allowed_origins'          => [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-    ],
+    'allowed_origins'          => array_merge(
+        explode(',', env('FRONTEND_URL')),
+        [
+            'http://localhost:3000',
+            'http://127.0.0.1:3000',
+            'http://localhost:5173',
+            'http://127.0.0.1:5173',
+        ]),
 
     'allowed_origins_patterns' => [],
 
